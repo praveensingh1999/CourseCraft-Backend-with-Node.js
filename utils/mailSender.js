646125eee152e78e42db182c9434d18aa0f8c7cd
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2026-02-06 20:42:37 +0530

    Update mailSender.js

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -4,16 +4,16 @@ export const mailSender = async (email, title, body) => {
   try {
     const transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
-      port: 587,
+      port: Number(process.env.MAIL_PORT),
       secure: false,
       auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
+        user: process.env.MAIL_USER, // apikey
+        pass: process.env.MAIL_PASS, // Brevo SMTP key
       },
     });
 
     const info = await transporter.sendMail({
-      from: `"Course Craft" <${process.env.MAIL_USER}>`,
+      from: `"Course Craft" <${process.env.MAIL_FROM}>`, 
       to: email,
       subject: title,
       html: body,
@@ -21,7 +21,7 @@ export const mailSender = async (email, title, body) => {
 
     return info;
   } catch (error) {
-    console.error("MailSender Error:", error.message);
+    console.error("MailSender Error:", error);
     throw error;
   }
 };
